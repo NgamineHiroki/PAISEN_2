798c1652e1e1bd48c3a7aadda19c3eeeec69df57
NagamineHiroki <weezy.23633@gmail.com>
2021-02-19 09:38:35 +0900

    Initial Commit

---
+++ PAISEN_2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PAISEN_2
+//
+//  Created by 永峯大暉 on 2021/02/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PAISEN_2/PAISEN_2App.swift
@@ -0,0 +1,17 @@
+//
+//  PAISEN_2App.swift
+//  PAISEN_2
+//
+//  Created by 永峯大暉 on 2021/02/19.
+//
+
+import SwiftUI
+
+@main
+struct PAISEN_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
